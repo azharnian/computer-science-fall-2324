7cc26edf66f963493fa97dedb737d60a1df0a36b
azharnian <anas.sty@gmail.com>
2023-08-23 16:58:11 +0700

    docs :

---
+++ 10INFORMATIKA/10IPA2/Meet02/who_are_you.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    cout << "What's your name ? ";
+    cin >> name;
+    cout << "Hi " << name << endl;
+    return 0;
+}
