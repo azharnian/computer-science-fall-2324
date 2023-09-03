0473c75241fcbdfd9d6c3c913ec2a4f0a43de3ae
azharnian <anas.sty@gmail.com>
2023-09-04 06:52:33 +0700

    add merge

--- MISC/ANAS/plotting/die_visual.py
+++ MISC/ANAS/plotting/die_visual.py
@@ -7,19 +7,21 @@ die = Die()
 
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die.roll() + die.roll()
     results.append(result)
 
-freqs = []
-for value in range(1, die.num_sides+1):
-    freq = results.count(value)
-    freqs.append(freq)
+print(results)
 
-x_values = list(range(1, die.num_sides+1))
-data = [Bar(x=x_values, y=freqs)]
+# freqs = []
+# for value in range(1, die.num_sides+1):
+#     freq = results.count(value)
+#     freqs.append(freq)
 
-x_axis_config = {"title" : "Result"}
-y_axis_config = {"title" : "Frequency of Result"}
-my_layout = Layout(title="Results of rolling one D6 1000 times", xaxis=x_axis_config, yaxis=y_axis_config)
+# x_values = list(range(1, die.num_sides+1))
+# data = [Bar(x=x_values, y=freqs)]
 
-offline.plot({"data":data, "layout":my_layout}, filename="d6.html")
+# x_axis_config = {"title" : "Result"}
+# y_axis_config = {"title" : "Frequency of Result"}
+# my_layout = Layout(title="Results of rolling one D6 1000 times", xaxis=x_axis_config, yaxis=y_axis_config)
+
+# offline.plot({"data":data, "layout":my_layout}, filename="d6.html")
