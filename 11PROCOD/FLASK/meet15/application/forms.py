977f8e86363707a5ded7a13b41139eaf8c9ebbc3
azharnian <anas.sty@gmail.com>
2023-11-09 16:32:12 +0700

    09/11

--- 11PROCOD/FLASK/meet15/application/forms.py
+++ 11PROCOD/FLASK/meet15/application/forms.py
@@ -19,11 +19,13 @@ class SignUpForm(FlaskForm):
     confirm_password    = PasswordField("confirm password", validators=[DataRequired(), Length(min=8), EqualTo("password")])
     submit              = SubmitField("sign up")
 
-class EditProfileForm(FlaskForm):
-    username            = StringField("username", validators=[DataRequired(), Length(min=4, max=12), exists_username])
-    email               = EmailField("email", validators=[DataRequired(), Email(), exists_email])
-    profile_pic         = FileField("profile picture", validators=[FileAllowed(["jpg", "png", "jpeg"])])
-    password            = PasswordField("password", validators=[DataRequired(), Length(min=8)])
+class EditProfileForm(SignUpForm):
+    username            = StringField("username", validators=[DataRequired(), Length(min=4, max=12)])
+    password            = None
+    confirm_password    = None
+    email               = None
+    bio                 = StringField("bio")
+    profile_pic         = FileField("picture picture", validators=[FileAllowed(["jpg", "png", "jpeg"])])
     submit              = SubmitField("update profile")
 
 class ResetPasswordForm(FlaskForm):
--- 11PROCOD/FLASK/meet15/application/routes.py
+++ 11PROCOD/FLASK/meet15/application/routes.py
@@ -39,6 +39,31 @@ def profile(username):
     reverse_posts = posts[::-1]
     return render_template('profile.html', title=f'{current_user.fullname} Profile', posts=reverse_posts)
 
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditProfileForm()
+
+    if form.validate_on_submit():
+        user = User.query.get(current_user.id)
+        if form.username.data != user.username:
+            user.username = form.username.data
+        user.fullname = form.fullname.data
+        user.bio = form.bio.data
+
+        if form.profile_pic.data:
+            pass
+
+        db.session.commit()
+        flash('Profile updated', 'success')
+        return redirect(url_for('profile', username=current_user.username))
+    
+    form.username.data = current_user.username
+    form.fullname.data = current_user.fullname
+    form.bio.data = current_user.bio
+    
+    return render_template('edit.html', title=f'Edit {current_user.username} Profile', form=form)
+
 @app.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
@@ -71,6 +96,3 @@ def signup():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
-
-if __name__ == '__main__':
-    app.run(debug=True)
