aa6823119657961aa3c48ee35ec2c9bc2207c1c6
azharnian <anas.sty@gmail.com>
2023-11-13 15:48:45 +0700

    13/11

--- 11PROCOD/FLASK/meet15/application/routes.py
+++ 11PROCOD/FLASK/meet15/application/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, make_response, jsonify
 from flask_login import login_user, login_required, logout_user, current_user
 
 from application import app
@@ -96,3 +96,17 @@ def signup():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/like/<int:post_id>', methods=['POST'])
+@login_required
+def like(post_id):
+    like = Like.query.filter_by(user_id=current_user,post_id=post_id).fisrt()
+    if not like:
+        like = Like(user_id=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.sesion.commit()
+        return make_response(200, jsonify({"status" : True}))
+    
+    db.session.delete(like)
+    db.session.commit()
+    return make_response(200, jsonify({"status" : False}))
